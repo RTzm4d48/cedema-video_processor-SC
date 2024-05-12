1cdc2578dd3a960232c578d45d7a6eb51519b177
Edgar-ae <edgar.aggre@gmail.com>
2024-05-12 03:17:01 -0500

    implemente in index, fix whit the csr tokens

--- api_rest/views.py
+++ api_rest/views.py
@@ -3,28 +3,34 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 import os
+from myapp.models import guia, video
 
 # from django.shortcuts import render # type: ignore
 # Create your views here..
 class my_apis(viewsets.ModelViewSet):
 
+    # video = video.objects.all()
+
     @action(detail=False, methods=['post'], url_path='create_video')
     def create_video(self, request):
         if request.method == 'POST':
             title = request.data.get('title')
-            attach_file = request.FILES.get('attach_file')
+            # attach_file = request.FILES.get('attach_file')
             num_item = request.data.get('num_item')
             old_title = request.data.get('old_title')
 
-            binary_data = attach_file.read()
-            print(attach_file)
-
         print('Hola desde la consola')
-
-        self.write_file(request, title, num_item, old_title, attach_file)
-        _, extension = os.path.splitext(attach_file.name)
-        data = "{ titulo: '"+title+"', extencion: '"+extension+"' number: "+num_item+"'}";
-        return Response(data, status=status.HTTP_200_OK)
+        # if attach_file is None:
+        #     return Response('No hay archivo adjunto', status=status.HTTP_400_BAD_REQUEST)
+        
+        # _, extension = os.path.splitext(attach_file.name)
+        # code = "{ titulo: '"+title+"', extencion: '"+extension+"' number: "+num_item+"'}";
+        # self.write_file(request, title, num_item, old_title, attach_file)
+        # self.save_video(request, title, num_item, extension, code
+        code = {
+            'mensaje': '¡Hola desde tu API en Django! MrBestia',
+        }
+        return Response(code, status=status.HTTP_200_OK)
 
 
     def get_queryset(self):
@@ -35,3 +41,28 @@ class my_apis(viewsets.ModelViewSet):
         with open(f'static/tmp/{num_item}_{title}{extension}', 'wb') as f:
             for chunk in attach_file.chunks():
                 f.write(chunk)
+
+    @action(detail=False, methods=['get'], url_path='deleted_video')
+    def deleted_video(self, request):
+        video.objects.all().delete()
+        # myvideo = video.objects.all()
+        # for i in myvideo:
+        #     print(i.title)
+        
+        return Response('Video eliminado', status=status.HTTP_200_OK)
+    
+    def save_video(self, request, title, num_item, extension, code):
+        new_video = video(title=title, num_items=num_item, extension=extension, code=code, id_guia=guia.objects.get(name='ES-Guia-v'))
+        new_video.save()
+
+    @action(detail=False, methods=['get'], url_path='select_videos')
+    def select_videos(self, request):
+        myvideo = video.objects.all()
+        data = []
+        for i in myvideo:
+            print(i.title)
+            data.append({'title': i.title, 'num_items': i.num_items, 'extension': i.extension, 'code': i.code, 'id_guia': i.id_guia.name})
+
+        resultados_json = list(data)
+
+        return Response(resultados_json, status=status.HTTP_200_OK)
--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,25 @@
 // Initialize the input
-document.getElementById('id_total_items').addEventListener('input', function() {
+var input = document.getElementById('id_total_items');
+input.addEventListener('input', function() {
     const inputValue = input.value;
     capture_num_item(inputValue);
 });
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // ¿El cookie comienza con el nombre que queremos?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
 // Define your function
 function capture_num_item(value) {
@@ -44,6 +61,88 @@ class PROCESS_DATA {
         console.log(this.file.files[0].name);
         console.log(this.tittle.value);
 
-        
+        var self = this; // Guarda una referencia a `this`
+
+        $(document).ready(function() {
+            // var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+            var csrftoken = getCookie('csrftoken');
+            var formData = new FormData();
+            formData.append('title', 'Titulo del video');
+            formData.append('num_item', '9');
+            formData.append('old_title', 'Titulo del video viejo');
+            // formData.append('attach_file', self.file.files[0]);
+            $.ajax({
+                url: '/api/my_apis/create_video/',
+                type: 'POST',
+                data: formData,
+                beforeSend: function(xhr) {
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                },
+                success: function(data) {
+                    console.log('Datos de la API AAAAAAAAAAAAAAAA:', data);
+                },
+                error: function(xhr, textStatus, errorThrown) {
+                    // Manejar errores si la solicitud falla
+                    console.error('Error al obtener datos de la API:', errorThrown);
+                }
+            });
+        });
     }
   }
+init_program();
+function init_program() {
+
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            url: '/api/my_apis/select_videos/',
+            type: 'GET',
+            success: function(data) {
+                console.log('Datos obtenidos de la API XD:', data);
+                pinter_data(data);
+            },
+            error: function(xhr, textStatus, errorThrown) {
+                console.error('Error al obtener datos de la API:', errorThrown);
+                reject(errorThrown);
+            }
+        });
+    });
+
+}
+
+function pinter_data(data){
+    for (var i = 0; i < data.length; i++) {
+        var title = data[i].title;
+        var code = data[i].code;
+        var extension = data[i].extension;
+        var num_items = data[i].num_items;
+
+        console.log(title);
+
+        var html_table = `
+        <tr class="ligth">
+            <th class="organism_th_1 tittle_cont">
+                <div class="icon">
+                    <img width="26px" height="23" src="static/img/icon_vid.png" alt="">
+                </div>
+                <div class="tittle">
+                    <p>${title}${extension}</p>
+                    <span>
+                        <p class="before">Before:</p>
+                        <p>vid_low_01</p>
+                    </span>
+                </div>
+            </th>
+            <th class="organism_th_2"><p>${num_items}</p></th>
+            <th class="organism_th_3">
+                <div class="actions">
+                    <img height="16px" src="static/img/icon_edit.png" alt="">
+                    <img height="16px" src="static/img/icon_deleted.png" alt="">
+                </div>
+            </th>
+        </tr>
+        `;
+        document.getElementById('table_videos').innerHTML += html_table;
+        document.getElementById('insert_code').innerHTML += `${code},<br>`;
+
+    }
+}
